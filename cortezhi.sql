c89143599b4542dd60c4800e8ca282afc340c951
Stumpp-KUN <kiril.bychkov.03@mail.ru>
2023-04-10 19:45:59 +0300

    added sql script,jar

---
+++ cortezhi.sql
@@ -0,0 +1,4 @@
+INSERT INTO employeedesc.employee(name,surname) VALUES ("testName","testSurname");
+INSERT INTO employeedesc.employee(name,surname) VALUES ("testName2","testSurname2");
+INSERT INTO employeedesc.task(completed,completion_date,description,end_date,start_date,employee_id) VALUES (false,"2023-01-12","testTaskFor2.0","2023-01-5","2023-01-5",1);
+INSERT INTO employeedesc.task(completed,completion_date,description,end_date,start_date,employee_id) VALUES (false,"2023-01-7","testTaskFor2.1","2023-01-2","2023-01-7",1);
